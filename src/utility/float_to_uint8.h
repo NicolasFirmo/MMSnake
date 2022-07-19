dfd380ff1df0909b1c8f0d107145a95de3c8bf71
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-07-19 18:06:47 -0300

    Fixing return type of floatToUint8

--- src/utility/float_to_uint8.h
+++ src/utility/float_to_uint8.h
@@ -1,5 +1,5 @@
 #pragma once
 
-[[nodiscard]] constexpr Uint8 floatToUint8(float normalized) {
+[[nodiscard]] constexpr uint8_t floatToUint8(float normalized) {
 	return 0xff * std::clamp(normalized, 0.0F, 1.0F);
 }
