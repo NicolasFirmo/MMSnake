3202b1048e267060ea4b345ff6fa13b9d91b7db8
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-07-21 20:10:15 -0300

    Smooth line rendering

--- src/assets/shaders/basic/basic.frag.glsl
+++ src/assets/shaders/basic/basic.frag.glsl
@@ -6,15 +6,15 @@ in vec2 v_LocalPosition;
 in float v_Thickness;
 
 void main() {
+	float fade = 0.005;
+
 	float alpha = 1;
 	if (v_LocalPosition.x < -1) {
-		if (distance(v_LocalPosition, vec2(-1, 0)) > v_Thickness) {
-			alpha = 0;
-		}
+		alpha = smoothstep(0.0, fade, v_Thickness - distance(v_LocalPosition, vec2(-1, 0)));
 	} else if (v_LocalPosition.x > 1) {
-		if (distance(v_LocalPosition, vec2(1, 0)) > v_Thickness) {
-			alpha = 0;
-		}
+		alpha = smoothstep(0.0, fade, v_Thickness - distance(v_LocalPosition, vec2(1, 0)));
+	} else {
+		alpha = smoothstep(0.0, fade, v_Thickness - length(v_LocalPosition.y));
 	}
 	color = vec4(0, 0, 0, alpha);
 }
