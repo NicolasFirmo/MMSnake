345f63f646c9ae17d292ca5a863dc5572a67c852
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-07-24 19:43:37 -0300

    Taking out the rendering of the fade
    The smoothing of the line edges will be render later in anti-aliasing.

--- src/assets/shaders/line/line.frag.glsl
+++ src/assets/shaders/line/line.frag.glsl
@@ -6,15 +6,15 @@ in vec2 v_LocalPosition;
 in float v_LocalLength;
 
 void main() {
-	float fade = 0.1;
-
 	float alpha = 1;
 	if (v_LocalPosition.x < -v_LocalLength) {
-		alpha = smoothstep(0.0, fade, 1 - distance(v_LocalPosition, vec2(-v_LocalLength, 0)));
+		if (distance(v_LocalPosition, vec2(-v_LocalLength, 0)) > 1) {
+			alpha = 0;
+		}
 	} else if (v_LocalPosition.x > v_LocalLength) {
-		alpha = smoothstep(0.0, fade, 1 - distance(v_LocalPosition, vec2(v_LocalLength, 0)));
-	} else {
-		alpha = smoothstep(0.0, fade, 1 - length(v_LocalPosition.y));
+		if (distance(v_LocalPosition, vec2(v_LocalLength, 0)) > 1) {
+			alpha = 0;
+		}
 	}
 	color = vec4(0, 0, 0, alpha);
 }
