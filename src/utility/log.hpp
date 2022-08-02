403cb5dc8b5a67e062b5c997e7c3928c2c3e49a3
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-08-02 01:15:49 -0300

    Suppressing MSVC warning for debugLog in release build

--- src/utility/log.hpp
+++ src/utility/log.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4100)
+#endif
+
 template <typename... T>
 constexpr void debugLog(fmt::format_string<T...> fmt, T &&...args) {
 	if constexpr (DEBUG)
 		fmt::print(std::move(fmt), std::forward<T>(args)...);
 }
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
