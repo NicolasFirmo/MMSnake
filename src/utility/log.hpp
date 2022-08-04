32ead569287c832b7b2a7fbd1e92e06fb27a8d3a
Nicolas Firmo <49406269+NicolasFirmo@users.noreply.github.com>
2022-08-04 18:13:24 -0300

    Added color to debug logs

--- src/utility/log.hpp
+++ src/utility/log.hpp
@@ -8,7 +8,7 @@
 template <typename... T>
 constexpr void debugLog(fmt::format_string<T...> fmt, T &&...args) {
 	if constexpr (DEBUG)
-		fmt::print(std::move(fmt), std::forward<T>(args)...);
+		fmt::print(fmt::fg(fmt::color::light_green),std::move(fmt), std::forward<T>(args)...);
 }
 
 #if defined(_MSC_VER)
