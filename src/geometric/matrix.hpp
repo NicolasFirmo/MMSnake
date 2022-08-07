76c4ab5c98ac07164393e5a516320bdc659eb6f5
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-08-07 19:36:46 -0300

    Added Matrix4 identity and orthographic factories

--- src/geometric/matrix.hpp
+++ src/geometric/matrix.hpp
@@ -8,6 +8,39 @@ struct Matrix4 {
 
 	Row r[4];
 
+	[[nodiscard]] static constexpr Matrix4 identity() {
+		return {
+			{
+				{{1, 0, 0, 0}},
+				{{0, 1, 0, 0}},
+				{{0, 0, 1, 0}},
+				{{0, 0, 0, 1}},
+			 }
+		 };
+	}
+
+	[[nodiscard]] static constexpr Matrix4 orthographic(const T& left, const T& right,
+													   const T& bottom, const T& up) {
+		constexpr T front = -1;
+		constexpr T rear  = 1;
+
+		const T midX = (left + right) / 2;
+		const T midY = (bottom + up) / 2;
+		const T midZ = (-rear + -front) / 2;
+
+		const T scaleX = 2 / (right - left);
+		const T scaleY = 2 / (up - bottom);
+		const T scaleZ = 2 / (rear - front);
+
+		return {
+			{
+				{{scaleX, 0, 0, -midX * scaleX}},
+				{{0, scaleY, 0, -midY * scaleY}},
+				{{0, 0, scaleZ, -midZ * scaleZ}},
+				{{0, 0, 0, 1}},
+			 }
+		 };
+	}
 	[[nodiscard]] constexpr Matrix4 operator+(const Matrix4& other) const {
 		Matrix4 res;
 		for (size_t i = 0; i < 4; i++)
